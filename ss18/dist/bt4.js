1978e5d6a6ff792c7a8e7b1a143110589e4c3cd3
Thuận NM <150407855+kenduNMT@users.noreply.github.com>
2024-05-30 09:49:51 +0700

    Update bt4.js

--- ss18/dist/bt4.js
+++ ss18/dist/bt4.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function validateDecorator(validationFn) {
     return function (target, propertyKey, descriptor) {
         const originalMethod = descriptor.value;
@@ -17,43 +23,48 @@ function validateDecorator(validationFn) {
 function validatePositiveNumbers(args) {
     return args.every(arg => typeof arg === 'number' && arg > 0);
 }
-// Sử dụng hàm decorator với hàm validatePositiveNumbers
-const validatedAdd = validateDecorator(validatePositiveNumbers)(null, "validatedAdd", {
-    value: (a, b) => {
+// Sử dụng decorator @validateDecorator với hàm validatePositiveNumbers
+class Calculator {
+    add(a, b) {
         return a + b;
-    },
-}).value;
-// Gọi hàm với các tham số hợp lệ
-try {
-    console.log(validatedAdd(2, 3));
-}
-catch (error) {
-    console.error(error.message);
-}
-// Gọi hàm với các tham số không hợp lệ
-try {
-    console.log(validatedAdd(-1, 3));
-}
-catch (error) {
-    console.error(error.message);
-}
-// Thêm ví dụ hàm không cần class
-const validatedMultiply = validateDecorator(validatePositiveNumbers)(null, "validatedMultiply", {
-    value: (a, b) => {
+    }
+    multiply(a, b) {
         return a * b;
-    },
-}).value;
+    }
+}
+__decorate([
+    validateDecorator(validatePositiveNumbers)
+], Calculator.prototype, "add", null);
+__decorate([
+    validateDecorator(validatePositiveNumbers)
+], Calculator.prototype, "multiply", null);
+const calculator = new Calculator();
 // Gọi hàm với các tham số hợp lệ
 try {
-    console.log(validatedMultiply(4, 5));
+    console.log(calculator.add(2, 3));
 }
 catch (error) {
     console.error(error.message);
 }
 // Gọi hàm với các tham số không hợp lệ
 try {
-    console.log(validatedMultiply(4, -5));
+    console.log(calculator.add(-1, 3));
 }
 catch (error) {
     console.error(error.message);
 }
+// Gọi hàm với các tham số hợp lệ
+try {
+    console.log(calculator.multiply(4, 5));
+}
+catch (error) {
+    console.error(error.message);
+}
+// Gọi hàm với các tham số không hợp lệ
+try {
+    console.log(calculator.multiply(4, -5));
+}
+catch (error) {
+    console.error(error.message);
+}
+    
